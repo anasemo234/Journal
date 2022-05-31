ae1c3e767626eba8b3d7060657eba77fa52e6d49
Anasemos Kassahun <Anasemosaka@gmail.com>
2022-05-31 16:27:53 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+const mongoose = require('mongoose');
+const express = require("express");
+const methodOverride = require("method-override");
+const app = express();
+const PORT = 3000;
+
+mongoose.connect(process.env.DATABASE_URL, {
+    useUnifiedTopology: true,
+});
+
+db = mongoose.connection;
+db.on('error', (err) => {
+    console.log(`${err.message} is mongodb not connected?`)
+});
+db.on('connected', () => console.log("MONGO is connected?"));
+db.on('disconnected', () => console.log('mongo has disconnected'));
+
+app.use(methodOverride("_method"));
+app.use(express.urlencoded({ extended: false }));
+
+app.listen(PORT, () => {
+    console.log(`;istening on port ${PORT}`);
+});
